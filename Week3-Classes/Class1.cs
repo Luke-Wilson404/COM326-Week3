8a4dbc95d70463417a0a1988c6f5af9094639d2a
csf23wil <csf23wil@MF124PC01>
2024-10-08 11:22:30 +0100

    Changes at 11:23

--- Week3-Classes/Class1.cs
+++ Week3-Classes/Class1.cs
@@ -3,42 +3,12 @@ using System;
 
 namespace Program
 {
-    class StudentLibrary()
+    class Student()
     {
-        private int id;
-        private string name;
-        private int age;
-        private static int Studentcount = 0;
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        public int Age
-        {
-            get { return age; }
-            set { age = value; }
-        }
-
-        public int ID
-        {
-            get { return id; }
-        }
-
-        public static int StudentCount
-        {
-            get { return Studentcount; }
-        }
-
-        public void Details(int id, string name, int age, int studentCount)
-        {
-            this.name = "John Doe";
-            this.age = 16;
-            Studentcount = 1;
-            this.id = Studentcount++;
-        }
+        int id;
+        string name;
+        int age;
+        static int Studentcount = 0;
 
         public void Display()
         {
--- Week3-Classes/Program.cs
+++ Week3-Classes/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.Net.Cache;
 
 namespace Program
 {
     class Book
     {
+        //Properties
         string Title;
         string Author;
         string ISBN;
         int NoOfPages;
+
+        //Constructor for book object
+        public Book(string bookTitle, string bookAuthor, string bookISBN, int bookPages)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+            NoOfPages = bookPages;
+        }
 
         void DisplayInfo()
         {
@@ -24,24 +35,15 @@ namespace Program
         static void Main(string[] args)
         {
             //Create a new instance of the book class
-            Book book1 = new Book();
-
-            book1.Title = "C# for Beginners";
-            book1.Author = "Bill Gates";
-            book1.ISBN = "123748920";
-            book1.NoOfPages = 200;
-
-            //Create a new instance of the book class
-            Book book2 = new Book();
-
-            book2.Title = "Ultimate C#";
-            book2.Author = "Microsoft";
-            book2.ISBN = "123748921";
-            book2.NoOfPages = 50;
+            Book book1 = new Book("C# for Beginners", "Bill Gates", "122", 200);
+            Book book2 = new Book("Visual Studio 2022", "Microsoft", "123", 70);
 
             //Output book information to the console
             book1.DisplayInfo();
             book2.DisplayInfo();
+
+            Student student1 = new Student();
+            Student student2 = new Student();
 
         }
     }
